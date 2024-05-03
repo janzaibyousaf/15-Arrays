e6e2902a001495d479a518330ca4a763d6e1b6d6
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-05-03 19:58:31 +0500

    Add files via upload

---
+++ ARRAYS/app.js
@@ -0,0 +1,26 @@
+var student1 = "Janzaib";
+var student2 = "Yousaf";
+
+var students = ["Janzaib"," Yousaf","Khokher"];
+
+students[3] = "Naseeb";
+students[4] = "Khan";
+
+console.log(students);
+
+var students = ["zahid","majid","riyaz","salman"]
+var b = students.slice(0,3)
+
+// console.splice
+console.log(b)
+
+var students = ["zahid","majid","riyaz","salman"]
+
+students.splice(1,1,)
+
+console.log(students)
+var a = ["muhmmad","fazal","naseeb","janzaib"]
+
+for (var i = 0; i < a.lenght ; i++){
+    console.log(a [i])
+}
